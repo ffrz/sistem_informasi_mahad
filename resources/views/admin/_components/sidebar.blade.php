8f53b2fb3988c0ba68deedc7209fb27e36972b03
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-05-23 21:34:57 +0700

    Update sidebar.blade.php

--- resources/views/admin/_components/sidebar.blade.php
+++ resources/views/admin/_components/sidebar.blade.php
@@ -7,16 +7,16 @@ if (!isset($menu_active)) {
 
 ?>
 <aside class="main-sidebar sidebar-light-primary elevation-4">
-  <a href="{{ url('/admin/') }}" class="brand-link">
-    <img src="{{ url('dist/img/logo.png') }}" alt="App Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
+  <a href="{{ url('/admin') }}" class="brand-link">
+    {{-- <img src="{{ url('dist/img/logo.png') }}" alt="App Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
+    <span class="brand-text">{{ env('APP_NAME') }}</span>
   </a>
   <div class="sidebar">
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column nav-flat nav-flat nav-collapse-hide-child" data-widget="treeview"
         role="menu" data-accordion="false">
         <li class="nav-item">
-          <a href="{{ url('/admin/') }}" class="nav-link {{ $nav_active == 'dashboard' ? 'active' : '' }}">
+          <a href="{{ url('/admin') }}" class="nav-link {{ $nav_active == 'dashboard' ? 'active' : '' }}">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>Dashboard</p>
           </a>
